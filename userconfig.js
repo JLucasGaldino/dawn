a857beba2756c88b110beca215e47a16e64ec0b0
JLucasGaldino <joaolngaldino@gmail.com>
2024-02-21 11:45:03 +0000

    Try again

--- userconfig.js
+++ userconfig.js
@@ -15,7 +15,6 @@ const CONFIG = new Config({
   },
   search: {
     engines: {
-      g: ['https://google.com/search?q=', 'Google'],
       i: ['https://ixquick.com/do/search?q=', 'Ixquick'],
       d: ['https://duckduckgo.com/html?q=', 'DuckDuckGo'],
       y: ['https://youtube.com/results?search_query=', 'Youtube'],
